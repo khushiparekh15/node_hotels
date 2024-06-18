5049431cdcfea90ec5de59b4950e5d39a424bfc8
unknown <khushiparekh1507@gmail.com>
2024-06-18 16:14:04 +1200

    shift mongodb

--- server.js
+++ server.js
@@ -2,13 +2,21 @@ const express = require('express')
 const app = express();
 const db =require('./db');
 require('dotenv').config();
+const passport = require('passport');
+const Localstrategy = require('passport-local').Strategy;
+
 
 const bodyParser =require('body-parser');
 app.use(bodyParser.json());
 
 const PORT = process.env.PORT ||3000;
 
-
+//Middleware Function........
+const logRequest=(req,res,next)=>{
+        console.log(`[${new Date().toLocaleString()}] Request Made to : ${req.originalUrl}`);
+        next();
+}
+app.use(logRequest);
 app.get('/',function(req,res){
         res.send('Welcome to my hotel')
 })
